cb87509713df2832d3308ab08439eb12901955c9
nex5turbo <50979183+nex5turbo@users.noreply.github.com>
2022-12-23 17:12:43 +0900

    complete CircularTextButton

--- Sources/WYWidgets/CircularTextButton.swift
+++ Sources/WYWidgets/CircularTextButton.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 public struct CircularTextButton: View {
-    var title: String = ""
-    var action: () -> Void
-    public init(title: String = "", action: @escaping () -> Void) {
+    private var title: String
+    private var action: () -> Void
+    public init(_ title: String, action: @escaping () -> Void) {
         self.title = title
         self.action = action
     }
@@ -18,50 +18,75 @@ public struct CircularTextButton: View {
     private var backgroundColor: Color = .clear
     private var tintColor: Color? = nil
     private var fontSize: CGFloat = 15
-    
+    private var textPadding: CGFloat? = nil
+    private var strokeColor: Color = .clear
+    private var strokeLineWidth: CGFloat = 1
+
+    @State private var circleSize: CGFloat = 0
 
     public var body: some View {
         
         Button {
-         
             action()
         } label: {
-
             ZStack {
-                
-                backgroundColor
+                Circle()
+                    .strokeBorder(strokeColor, lineWidth: strokeLineWidth)
+                    .background(backgroundColor)
+                    .frame(width: circleSize, height: circleSize)
+
                 Text(title)
                     .font(.system(size: fontSize))
                     .foregroundColor(tintColor)
+                    .padding(.all, textPadding)
+                    .overlay(
+                        GeometryReader { proxy in
+                            Color.clear
+                                .onAppear {
+                                    self.circleSize = max(proxy.size.height, proxy.size.width)
+                                }
+                                .onChange(of: proxy.size) { newValue in
+                                    self.circleSize = max(proxy.size.height, proxy.size.width)
+                                }
+                        }
+                    )
             }
-            .contentShape(Circle())
+            .clipShape(Circle())
+            .clipped()
         }
     }
 }
 
 extension CircularTextButton {
-
-    func setBackgroundColor(_ color: Color) -> Self {
+    
+    public func setBackgroundColor(_ color: Color) -> Self {
         var newView = self
         newView.backgroundColor = color
         return newView
     }
-
-    func fontSize(_ fontSize: CGFloat) -> Self {
+    
+    public func fontSize(_ fontSize: CGFloat) -> Self {
         var newView = self
         newView.fontSize = fontSize
         return newView
     }
-
-    func tintColor(_ color: Color) -> Self {
+    
+    public func tintColor(_ color: Color) -> Self {
         var newView = self
         newView.tintColor = color
         return newView
     }
-}
+    
+    public func textPadding(_ value: CGFloat) -> Self {
+        var newView = self
+        newView.textPadding = value
+        return newView
+    }
 
-struct CircularTextButton_Previews: PreviewProvider {
-    static var previews: some View {
-        CircularTextButton { }
+    public func stroke(_ color: Color, lineWidth: CGFloat = 1) -> Self {
+        var newView = self
+        newView.strokeColor = color
+        newView.strokeLineWidth = lineWidth
+        return newView
     }
 }
